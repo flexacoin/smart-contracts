2a1238d7ce30ff145fcd269ec063c02ff5d6d999
Zachary Kilgore <zkilgore@gmail.com>
2018-04-17 12:22:09 -0400

    Lint and refactor

--- contracts/Recoverable.sol
+++ contracts/Recoverable.sol
@@ -6,24 +6,24 @@ import "../zeppelin/contracts/token/ERC20/SafeERC20.sol";
 
 
 contract Recoverable is Claimable {
-    using SafeERC20 for ERC20Basic;
+  using SafeERC20 for ERC20Basic;
 
-    /**
-     * @dev Recover all ERC20Basic compatible tokens
-     * @param token ERC20Basic The address of the token contract
-     */
-    function recoverToken(ERC20Basic token) external onlyOwner {
-        uint256 balance = token.balanceOf(this);
-        token.safeTransfer(owner, balance);
-    }
+  /**
+    * @dev Recover all ERC20Basic compatible tokens
+    * @param token ERC20Basic The address of the token contract
+    */
+  function recoverToken(ERC20Basic token) external onlyOwner {
+    uint256 balance = token.balanceOf(this);
+    token.safeTransfer(owner, balance);
+  }
 
-    /**
-     * @dev Interface function, can be overwritten by the superclass
-     * @param token Token which balance we will check and return
-     * @return The amount of tokens (in smallest denominator) the contract owns
-     */
-    function tokensToBeReturned(ERC20Basic token) public view returns (uint) {
-        return token.balanceOf(this);
-    }
+  /**
+    * @dev Interface function, can be overwritten by the superclass
+    * @param token Token which balance we will check and return
+    * @return The amount of tokens (in smallest denominator) the contract owns
+    */
+  function tokensToBeReturned(ERC20Basic token) public view returns (uint) {
+    return token.balanceOf(this);
+  }
 
 }
--- contracts/TokenVault.sol
+++ contracts/TokenVault.sol
@@ -12,54 +12,54 @@ import "../zeppelin/contracts/math/SafeMath.sol";
  * Contract is Recoverable, which also means it is Ownable
  */
 contract TokenVault is Recoverable {
-    using SafeMath for uint256;
-    using SafeERC20 for ERC20Basic;
+  using SafeMath for uint256;
+  using SafeERC20 for ERC20Basic;
 
-    event Locked();
-    event Unlocked();
+  event Locked();
+  event Unlocked();
 
-    // token address
-    address public token;
+  // token address
+  address public token;
 
-    /**
-     * tokensToBeAllocated should be set on initialization with the amount of
-     * tokens that should be allocated prior to releasing the tokens.
-     */
-    uint256 public tokensToBeAllocated;
-    uint256 public tokensAllocated;
+  /**
+    * tokensToBeAllocated should be set on initialization with the amount of
+    * tokens that should be allocated prior to releasing the tokens.
+    */
+  uint256 public tokensToBeAllocated;
+  uint256 public tokensAllocated;
 
-    // is the contract loaded or locked
-    bool public isLoaded;
-    bool public isLocked;
+  // is the contract loaded or locked
+  bool public isLoaded;
+  bool public isLocked;
 
-    // Mapping of accounts to token allocations
-    mapping (address => uint256) private allocations;
-    // Mapping of accounts to token balances
-    mapping (address => uint256) private bonuses;
-
-
-    /**
-     * @dev Creates a TokenVault contract that stores FlexaToken distribution and
-     *      bonus allocations and lockup.
-     */
-    function TokenVault(ERC20Basic _token) public {
-        require(_token != address(0));
-
-        token = _token;
-    }
-
-    /**
-     * @dev Function to set allocations for accounts. To be called by owner,
-     *      most likely by a script.
-     * @param _to The address to allocate token amount and bonus amount for.
-     */
-    function setAllocation(address _to, uint256 _value, uint256 _bonus) external returns(bool);
+  // Mapping of accounts to token allocations
+  mapping (address => uint256) private allocations;
+  // Mapping of accounts to token balances
+  mapping (address => uint256) private bonuses;
 
 
-    /**
-     * @dev Claim can be called by an account to claim whatever tokens for an
-     *      account are ready to be claimed.
-     */
-    function claim() public returns (bool);
+  /**
+    * @dev Creates a TokenVault contract that stores FlexaToken distribution and
+    *      bonus allocations and lockup.
+    */
+  function TokenVault(ERC20Basic _token) public {
+    require(_token != address(0));
+
+    token = _token;
+  }
+
+  /**
+    * @dev Function to set allocations for accounts. To be called by owner,
+    *      most likely by a script.
+    * @param _to The address to allocate token amount and bonus amount for.
+    */
+  function setAllocation(address _to, uint256 _value, uint256 _bonus) external returns(bool);
+
+
+  /**
+    * @dev Claim can be called by an account to claim whatever tokens for an
+    *      account are ready to be claimed.
+    */
+  function claim() public returns (bool);
 
 }
